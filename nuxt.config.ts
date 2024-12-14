e50373b45db8165b14ca27ab7f127363b787509f
Nick Ezekiel <nickezekias@gmail.com>
2024-12-14 14:11:39 +0100

    optimzed vue-i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,23 @@
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "url";
+import VueI18nVitePlugin from "@intlify/unplugin-vue-i18n/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ["vue-i18n"],
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   typescript: { typeCheck: true },
-  modules: ["@nuxt/eslint", "@pinia/nuxt"]
+  modules: ["@nuxt/eslint", "@pinia/nuxt"],
+  vite: {
+    plugins: [
+      VueI18nVitePlugin({
+        include: [
+          resolve(dirname(fileURLToPath(import.meta.url)), "./locales/*.json"),
+        ],
+      }),
+    ],
+  },
 });
