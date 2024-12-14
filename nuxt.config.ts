66ecb5b42baa2882f7ef2c5b657837f641af1f6c
Nick Ezekiel <nickezekias@gmail.com>
2024-12-14 11:06:00 +0100

    added code styling with eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  typescript: { typeCheck: true }
+  typescript: { typeCheck: true },
+  modules: ["@nuxt/eslint"]
 });
