867b177d2991962d14c153631b8d6f1fa4c25be1
Nick Ezekiel <nickezekias@gmail.com>
2024-12-14 15:45:29 +0100

    [WIP] Installed & Configured primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import { resolve, dirname } from "node:path";
 import { fileURLToPath } from "url";
 import VueI18nVitePlugin from "@intlify/unplugin-vue-i18n/vite";
+import Aura from "@primevue/themes/aura";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -10,7 +11,14 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   typescript: { typeCheck: true },
-  modules: ["@nuxt/eslint", "@pinia/nuxt"],
+  modules: ["@nuxt/eslint", "@pinia/nuxt", "@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
   vite: {
     plugins: [
       VueI18nVitePlugin({
