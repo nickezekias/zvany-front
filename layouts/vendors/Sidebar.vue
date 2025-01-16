9ae60ca4198ec56164d85eb09466178c38b0201a
Nick Ezekiel <nickezekias@gmail.com>
2025-01-17 00:45:17 +0100

    fixed sidebar active item trigger during navigation

--- layouts/vendors/Sidebar.vue
+++ layouts/vendors/Sidebar.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
+import { watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const accountStore = useAccountStore()
+const route = useRoute()
 const { t } = useI18n()
 
-const activeItem = ref(null)
-const openMenus = ref({}) // Tracks open menus at multiple levels
+interface MenuItem {
+  label: string
+  icon?: string
+  route?: string
+  children?: Array<MenuItem>
+  command?: () => void
+}
+
+const activeItem: Ref<MenuItem | null> = ref(null)
+const currentUrl = ref(route.path)
+const openMenus: Ref<Record<string, boolean>> = ref({}) // Tracks open menus at multiple levels
 
 const menuItems = ref([
-  { label: t('labels.dashboard'), icon: 'pi pi-home', url: '/vendors/' },
+  { label: t('labels.dashboard'), icon: 'pi pi-home', route: '/vendors/' },
   // { label: 'Profile', icon: 'pi pi-user' },
   {
     label: t('labels.product', 2),
@@ -21,6 +32,7 @@ const menuItems = ref([
           { label: 'Change Password', icon: 'pi pi-key' },
           { label: 'Two-Factor Authentication', icon: 'pi pi-shield' },
         ], */
+        route: '/vendors/products',
       },
       { label: t('labels.inventoryAdjustment', 2) },
     ],
@@ -30,53 +42,92 @@ const menuItems = ref([
     label: t('labels.license'),
     icon: 'pi pi-file',
     children: [
-      { label: t('labels.payment', 2) },
+      { label: t('labels.payment', 2), route: '/admin/payments' },
       // { label: 'Logout', icon: 'pi pi-sign-out' },
     ],
   },
 ])
 
 onMounted(() => {
-  menuItems.value.forEach((item) => {
-    if (item.url) {
-      //FIXME: see best ways to get current route with nuxt
-      if (item.url === window.location.pathname) {
-        activeItem.value = item
-      }
-    }
-    /* if (item.children) {
-      openMenus.value[`${0}-${item.label}`] = false
-      item.children.forEach((child) => {
-        if (child.children) {
-          openMenus.value[`${1}-${child.label}`] = false
-        }
-      })
-    } */
-  })
+  openAndActivateMenuItems()
 })
 
-function toggleSubMenu(item, level) {
+watch(
+  currentUrl,
+  () => {
+    openAndActivateMenuItems()
+  },
+  { deep: true, immediate: true }
+)
+
+
+function openAndActivateMenuItems() {
+  menuItems.value.forEach((item) => {
+    openMenus.value[`${0}-${item.label}`] = false
+    if (item.route) {
+      //FIXME: see best ways to get current route with nuxt
+      if (item.route === currentUrl.value) {
+        activeItem.value = item
+        return
+      }
+    }
+    if (item.children) {
+      item.children.forEach((child: MenuItem) => {
+        openMenus.value[`${1}-${child.label}`] = false
+        if (child.route) {
+          if (child.route === currentUrl.value) {
+            activeItem.value = child
+            openMenus.value[`${0}-${item.label}`] = true
+            return
+          }
+        }
+        if (child.children) {
+          child.children.forEach((subChild) => {
+            if (subChild.route) {
+              if (subChild.route === currentUrl.value) {
+                activeItem.value = subChild
+                openMenus.value[`${1}-${child.label}`] = true
+                return
+              }
+            }
+          })
+        }
+      })
+    }
+  })
+}
+
+function toggleSubMenu(item: MenuItem, level: number) {
+  // Close all other toggled menu items
+  Object.keys(openMenus.value).forEach(key => {
+    if (key != `${level}-${item.label}` && !item.route) {
+      openMenus.value[key] = false
+    }
+  })
+
   if (!item.children) {
     setActiveItem(item)
-    return
+    if (item.route) {
+      navigateTo(item.route)
+    }
   }
   const key = `${level}-${item.label}`
   openMenus.value[key] = !openMenus.value[key]
 }
 
-function isOpen(item, level) {
+function isOpen(item: MenuItem, level: number) {
   const key = `${level}-${item.label}`
   return !!openMenus.value[key]
 }
 
-function setActiveItem(item) {
+function setActiveItem(item: MenuItem) {
   activeItem.value = item
 }
 </script>
 <template>
   <div class="w-64 shadow-sm bg-white dark:bg-[#18181b]">
     <h1 class="text-2xl font-bold py-4 px-3">
-      {{ accountStore.user.value.business.name }}
+      {{ accountStore.user.value?.business?.name }}
     </h1>
     <ul class="p-2">
       <li v-for="item in menuItems" :key="item.label" class="mb-1">
@@ -103,8 +154,8 @@ function setActiveItem(item) {
           <li v-for="child in item.children" :key="child.label" class="mb-1">
             <button
               :class="[
-                'w-full flex items-center p-3 rounded-md transition-all text-sm',
-                activeItem === item
+                'w-full flex items-center p-3 py-2 rounded-md transition-all text-sm font-bold',
+                activeItem === child
                   ? 'text-[#0099ff] bg-gray-50 dark:bg-[#ffffff08] border border-[#ffffff12]'
                   : 'hover:bg-gray-50 dark:hover:bg-[#ffffff08] font-normal hover:font-semibold dark:hover:text-primary',
               ]"
@@ -130,8 +181,8 @@ function setActiveItem(item) {
               >
                 <button
                   :class="[
-                    'w-full flex items-center p-3 rounded-md transition-all text-sm',
-                    activeItem === item
+                    'w-full flex items-center p-3 py-2 rounded-md transition-all text-sm font-bold',
+                    activeItem === subChild
                       ? 'text-[#0099ff] bg-gray-50 dark:bg-[#ffffff08] border border-[#ffffff12]'
                       : 'hover:bg-gray-50 dark:hover:bg-[#ffffff08] font-normal hover:font-semibold dark:hover:text-primary',
                   ]"
