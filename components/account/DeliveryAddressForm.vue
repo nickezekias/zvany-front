cdfdc05f6b9598f1d4ceae46fe89f1243d40eb5c
Nick Ezekiel <nickezekias@gmail.com>
2025-02-08 01:32:50 +0100

    [WIP] feat: added checkout page with delivery address form component

---
+++ components/account/DeliveryAddressForm.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import NikkInputText from '@/components/forms/NikkInputText.vue'
+import NikkSelect from '~/components/forms/NikkSelect.vue'
+
+const state = ref({
+  address: '',
+  apartment: '',
+  city: '',
+  country: '',
+  firstName: '',
+  lastName: '',
+})
+</script>
+
+<template>
+  <form class="flex flex-col gap-4">
+    <NikkSelect
+      id="type"
+      v-model="state.country"
+      error-help-label="errors.validation.requiredField"
+      :is-error="false"
+      label="labels.country"
+      :options="['France', 'Gabon', 'Rwanda', 'USA']"
+    />
+    <div class="flex gap-4">
+      <NikkInputText
+        id="first-name"
+        v-model="state.firstName"
+        class="w-full"
+        error-help-label="errors.validation.requiredField"
+        :is-error="false"
+        label="labels.firstName"
+        type="text"
+      />
+
+      <NikkInputText
+        id="last-name"
+        v-model="state.lastName"
+        class="w-full"
+        error-help-label="errors.validation.requiredField"
+        :is-error="false"
+        label="labels.lastName"
+        type="text"
+      />
+    </div>
+
+    <NikkInputText
+      id="address"
+      v-model="state.address"
+      error-help-label="errors.validation.requiredField"
+      :is-error="false"
+      label="labels.address"
+      type="text"
+    />
+
+    <NikkInputText
+      id="apartment-or-room-number"
+      v-model="state.apartment"
+      error-help-label="errors.validation.requiredField"
+      :is-error="false"
+      label="labels.apartment"
+      type="text"
+    />
+
+    <div class="flex gap-4">
+      <NikkSelect
+        id="type"
+        v-model="state.city"
+        class="w-full"
+        error-help-label="errors.validation.requiredField"
+        :is-error="false"
+        label="labels.city"
+        :options="['Kigali', 'Libreville', 'Sacramento']"
+      />
+    </div>
+  </form>
+</template>
---
+++ pages/checkout.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import DeliveryAddressForm from '@/components/account/DeliveryAddressForm.vue'
+import NikkInputText from '@/components/forms/NikkInputText.vue'
+
+const email = ref('')
+</script>
+
+<template>
+  <div class="flex gap-8 px-12 lg:px-24 py-16">
+    <div class="w-1/2 flex flex-col gap-6">
+      <div>
+        <span class="text-bold text-xl">{{ $t('labels.contact') }}</span>
+        <NikkInputText
+          id="name"
+          v-model="email"
+          class="mt-4 mb-2"
+          error-help-label="errors.validation.requiredField"
+          :is-error="false"
+          label="labels.email"
+          type="email"
+        />
+        <span>{{ $t('labels.emailNewsAndOffers') }}</span>
+      </div>
+
+      <div class="mt-5">
+        <span class="text-bold text-xl">{{ $t('labels.delivery') }}</span>
+        <DeliveryAddressForm class="mt-6 mb-2" />
+        <span class="mt-4">{{ $t('labels.emailNewsAndOffers') }}</span>
+      </div>
+    </div>
+
+  </div>
+</template>
