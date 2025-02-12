e054c7beabd4ada3637222a3d757e3f9b3dc2276
Nick Ezekiel <nickezekias@gmail.com>
2025-02-13 00:00:41 +0100

    feat: update layout components for responsive design using utility classes

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,15 @@
 import TopBar from '~/layouts/TopBar.vue'
 import MobileTopBar from '~/layouts/mobile/TopBar.vue'
 import MobileBottomBar from '~/layouts/mobile/BottomBar.vue'
-import { useScreenSize } from '~/composables/useScreenSize'
-
-const { isMobile } = useScreenSize()
 </script>
 <template>
   <div class="h-screen w-screen flex-col">
     <PrimeToast />
   
-    <MobileTopBar v-if="isMobile" />
-    <TopBar v-else/>
+    <MobileTopBar class="md:hidden" />
+    <TopBar class="hidden md:flex"/>
     
-    <MobileBottomBar v-if="isMobile" />
+    <MobileBottomBar class="md:hidden" />
 
     <main>
       <slot />
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import MobileIndexContent from '~/components/pages/home/MobileIndexContent.vue';
 import IndexContent from '~/components/pages/home/IndexContent.vue';
-import { useScreenSize } from '~/composables/useScreenSize';
 
-const { isMobile } = useScreenSize()
 </script>
 <template>
   <div>
-    <MobileIndexContent v-if="isMobile" />
-    <IndexContent v-else />
+    <MobileIndexContent class="md:hidden" />
+    <IndexContent class="hidden md:flex" />
   </div>
 </template>
